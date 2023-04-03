3e4ca5015ea03cd3410265492062b7e66d24d08a
theliteprogrammer <219037801@student.uj.ac.za>
2023-04-04 01:56:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -24,27 +25,27 @@ public class Main {
      * @param n - The nth term of a fibonacci sequence
      * @return The remainder of the sum of all numbers in fibonacci sequence divided by 10
      */
-    private static long getFibonacciSumFast(long n){
+    private static BigInteger getFibonacciSumFast(long n){
         // create 1D array
         if (n <= 1) {
-            return n;
+            return BigInteger.valueOf(n);
         } else {
-            long[] array = new long[(int)n + 1];
-            array[0] = 0L;
-            array[1] = 1L;
-            long sum = 1L; // to calculate the sum - sum is 1 currently because 0 + 1 = 1
+            BigInteger[] array = new BigInteger[(int)n + 1];
+            array[0] = BigInteger.valueOf(0);
+            array[1] = BigInteger.valueOf(1);
+            BigInteger sum = BigInteger.valueOf(1); // to calculate the sum - sum is 1 currently because 0 + 1 = 1
             for(int i = 2; i < array.length; ++i) {
-                array[i] = array[i - 2] + array[i - 1];
-                sum += array[i];
+                array[i] = array[i - 2].add(array[i - 1]);
+                sum = sum.add(array[i]);
             }
-            return sum % 10;
+            return sum.mod(BigInteger.valueOf(10));
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumFast(n);
+        BigInteger s = getFibonacciSumFast(n);
         System.out.println(s);
     }
 }
